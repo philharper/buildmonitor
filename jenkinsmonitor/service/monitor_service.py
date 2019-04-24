0f4e925e8eb5bf0846910a90d78bf69cb8ddf711
Phil Harper <phil.harper@vistair.com>
2019-04-24 21:29:45 +0100

    Add second hard coded monitor

--- jenkinsmonitor/service/monitor_service.py
+++ jenkinsmonitor/service/monitor_service.py
@@ -2,13 +2,13 @@
 
 class MonitorService:
 
-    targeryen_jobs = ['VI Data Warehouse - Unit Test', 'Create Staging Tables', 'Run Stored Procedures',
+    targeryen_jobs = {'title': 'House Targaryen - VI', 'jobs': ['VI Data Warehouse - Unit Test', 'Create Staging Tables', 'Run Stored Procedures',
                  'VI Data Warehouse - Integration Test', 'VI Data Warehouse Test',
                  'Transformation Runner - DAL TC (UAT)', 'Transformation Runner - DAL (LIVE)',
-                 'Transformation Runner - DAL TC (LIVE)']
-    lannister_jobs = []
+                 'Transformation Runner - DAL TC (LIVE)']}
+    lannister_jobs = {'title': 'House Lannister - QualityNet', 'jobs': ['Deploy QualityNet to Test']}
 
-    def get_jobs_list(self, monitor):
+    def get_monitor(self, monitor):
         if monitor == 1:
             return self.targeryen_jobs
         if monitor == 2:
--- jenkinsmonitor/views.py
+++ jenkinsmonitor/views.py
@@ -13,9 +13,10 @@ def monitor(request, monitor_id):
     monitor_service = MonitorService()
 
     jenkins_jobs = []
+    monitor = monitor_service.get_monitor(monitor_id)
 
-    for job in monitor_service.get_jobs_list(monitor_id):
+    for job in monitor['jobs']:
         job = jenkins_service.get_jenkins_job(job)
         jenkins_jobs.append(job)
 
-    return render(request, 'jenkins/monitor.html', { 'jobs': jenkins_jobs })
+    return render(request, 'jenkins/monitor.html', {'jobs': jenkins_jobs, 'title': monitor['title']})
